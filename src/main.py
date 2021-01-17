ef46373e15f2a3ae4b4f732b85ccb15aa53256f2
arielmerin <arielmerino@ciencias.unam.mx>
2021-01-16 22:36:18 -0600

    terminal y números aleatorios
    Completé la primera parte de recibir argumentos desde la terminal y añadí la función que genera números aleatorios de 256 bits nomás

---
+++ src/main.py
@@ -0,0 +1,78 @@
+from sys import argv
+from shamir import Shamir
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+"""
+    Se encarga de recibir los argumentos en la terminal y manejar cualquier error por parte del usuario, al proveer indicaciones.
+    Regresa los valores necesarios para la ejecución en cada caso.
+"""
+
+def extract_arg():
+    try:
+        proceso = argv[1]
+        
+        if(proceso == "-c"):
+            archivo_por_cifrar = argv[2]
+            contrasenas_guardar = argv[3]
+            necesarios = argv[4]
+            evaluaciones = argv[5]
+            return [archivo_por_cifrar, contrasenas_guardar, necesarios, evaluaciones]
+             
+        elif(proceso == "-d"):
+            archivo_por_descifrar = argv[2]
+            contrasenas = argv[3]
+            return [archivo_por_descifrar, contrasenas]
+                      
+        elif(proceso == "-h"):
+            print("\nSecreto compartido de shamir")
+            
+            print( color.BOLD + color.BLUE + "\n\n-c\n\tCifrar\n\t------------------" + color.END)
+            print("\tDeben ingresarse los siguientes datos separados por espacios\n\t-archivo a cifrar\n\t-archivo para guardar contraseñas (txt), si no existe dicho archivo se creará\n\t-Evaluaciones necesarias para desencriptar el archivo\n\t-Evaluaciones deseadas\n")
+            print(color.UNDERLINE + "\tEjemplo:  python3 src/main.py -c InformeDeEstado.pdf contraseñas.txt 12 25" + color.END)
+            
+            print(color.RED +color.BOLD +"\n\n-d\n\tDescifrar\n\t------------------" + color.END)
+            print("\tDeben ingresarse los siguientes datos separados por espacios\n\t-archivo a descifrar\n\t-archivo donde se encuentren las contraseñas (en formato txt) separadas por saltos de linea")
+            print(color.UNDERLINE + "\tEjemplo:  python3 src/main.py -d InformeDeEstado.pdf contraseñas.txt" + color.END)
+            
+            print("\n\n-h\n\tObtener ayuda")
+        else:
+            raise ValueError
+    except ValueError:
+        print("Ingresa los argumentos correctos, encontrarás ayuda corriendo el programa con -h ")
+        exit()
+    except IndexError:
+        print("Ingresa los argumentos completos, encontrarás ayuda corriendo el programa con -h ")
+        exit()
+    else:
+        print("error")
+        exit()     
+    
+
+def main():
+    print("Hola mundo")
+    list_argumentos = extract_arg()
+    if(len(list_argumentos) == 4):
+        print('el proceso es: crifrar')
+        numero = Shamir.genera_coeficientes()
+        print(numero)
+        print(Shamir._primo)
+        
+        
+        
+    elif(len(list_argumentos) == 2):
+        print('el proceso es descrifrar')
+    else:
+        exit()    
+if __name__ == "__main__":
+    main()
---
+++ src/shamir.py
@@ -0,0 +1,34 @@
+import secrets
+import pyaes
+class Shamir:
+    _primo = 208351617316091241234326746312124448251235562226470491514186331217050270460481
+    
+        """
+        Genera numéros aletorioss de 256 bits, siempre encargándose de que dicho número sea menor al módulo primo
+        """
+    def genera_coeficientes():
+        numero_aleatorio = secrets.randbits(256)
+        
+        while(numero_aleatorio > Shamir._primo):
+            numero_aleatorio = secrets.randbits
+            
+        return numero_aleatorio
+    
+        """
+        docstring
+        """
+    def encriptar(archivo):
+        
+        pass
+    
+    
+        """
+        la lista_argumentos : [archivo_por_cifrar, contrasenas_guardar, necesarios, evaluaciones]
+        """
+    def cifrar(lista_argumentos):
+        archivo, contrasenas, necesarios, evaluaciones = lista_argumentos
+        
+        pass
+    
+    def descrifrar():
+        pass
